8ad7e95a254d760022c57012369951f2244cb21e
SillyPuffin <bailey.jl.stewart@gmail.com>
2023-06-28 20:07:12 +0100

    update

--- code/client/main.py
+++ code/client/main.py
@@ -1,18 +1,25 @@
-import pygame
+import pygame; from pygame.locals import *
+from sys import exit
 
 pygame.init()
 
-
+#main class
 class Main():
     def __init__(self):
-        self.size = (1080,1920)
-        self.screen = pygame.display.set_mode()
+        self.size = (1920,1080)
+        self.screen = pygame.display.set_mode(self.size)
 
 
     def run(self):
-        pass
+        while True:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                    pygame.quit()
+                    exit()
+            pygame.display.update()
 
 #running
-if __name__ == 'main.py':
+if __name__ == '__main__':
     game = Main()
     game.run()
