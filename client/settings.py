46697f6ad8094441bb179c39d60a8d1b24ea8ceb
SillyPuffin <89728058+SillyPuffin@users.noreply.github.com>
2023-06-30 14:14:17 +0100

    Delete client directory

--- client/settings.py
+++
@@ -1,3 +0,0 @@
-base_size = (480,270)
-tile_size = 20
-fps = 70
--- client/utils.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-def debug(s,scale,x=10,y=10):
-    string = str(s)
-    font = pygame.font.Font(None,10*scale)
-    text = font.render(string,True,(255,255,255))
-    screen = pygame.display.get_surface()
-    screen.blit(text,(x,y))
